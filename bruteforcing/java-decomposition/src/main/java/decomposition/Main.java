691a9e266da61b40b6f8ee3db59bbbb5254818f2
lovethefeel <lovethefeel@gmail.com>
2021-10-17 12:33:09 +0900

    refactor: 향상된 조회 메소드 추가

--- bruteforcing/java-decomposition/src/main/java/decomposition/Main.java
+++ bruteforcing/java-decomposition/src/main/java/decomposition/Main.java
@@ -1,19 +1,51 @@
 package decomposition;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        int input = new Scanner(System.in).nextInt();
-        int length = (int)(Math.log10(input)+1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String strInput = br.readLine();
+        int inputLen = strInput.length();
+        int input = Integer.parseInt(strInput);
 
-        int result = search(input, length);
+        int result = advanceSearch(input, inputLen);
         System.out.println(result);
     }
 
-    static int search(int input, int length) {
+    /**
+     * 0부터 생성자 찾을때까지 조회
+     * @param input int
+     * @return result
+     */
+    static int search(int input) {
         int result = 0;
         for( int i = 0; i<input; i++) {
+            int sum = 0;
+            int storeNum = i;
+            while(storeNum > 0) {
+                sum += storeNum % 10;
+                storeNum /= 10;
+            }
+            if( input == sum + i ) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 맥스값을 이용하여 생성자 조회
+     * @param input int
+     * @param size int
+     * @return result
+     */
+    static int advanceSearch(int input, int size) {
+        int result = 0;
+        for ( int i = (input - (size * 9)); i < input; i++ ) {
             int sum = 0;
             int storeNum = i;
             while(storeNum > 0) {
